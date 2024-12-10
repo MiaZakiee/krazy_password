cbb6556279ec567de9ea746a9d043da8edc10aad
miazakiee <ninocabiltes@gmail.com>
2024-12-10 23:28:00 +0800

    kani daw

--- script.js
+++ script.js
@@ -10,24 +10,23 @@ docdocument.getElementById("generate").addEventListener("click", function () {
   let lastFourDigits = id.slice(-4); // Get the last 4 characters
 
   // Combine the full name and remove spaces
-  let fullName = (firstName + " " + middleName + " " + lastName).replace(
-    /\s+/g,
-    ""
-  );
+  let fullName = (firstName + " " + " " + lastName).replace(/\s+/g, "");
   fullName = fullName.toUpperCase();
 
   // Extract first character of the last name
-  let firstCharLastName = lastName.trim().toUpperCase()[0];
+  //   let firstCharLastName = lastName.trim().toUpperCase()[0];
 
   // Calculate full name length minus 10
-  let adjustedLength = fullName.length - 10;
+  let adjustedLength = fullName.length - 1;
 
   // Extract the last character of the full name
-  let lastCharFullName = fullName[fullName.length - 1];
+  //   let lastCharFullName = fullName[fullName.length - 1];
+
+  let a = lastName.trim().toUpperCase()[0];
+  let b = firstName.trim().toUpperCase()[0];
 
   // Construct the password
-  let password =
-    lastFourDigits + firstCharLastName + adjustedLength + lastCharFullName;
+  let password = lastFourDigits + a + adjustedLength + b;
 
   // Display the password
   document.getElementById("password").innerText =
